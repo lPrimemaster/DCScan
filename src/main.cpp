50a6b3284acfc34dd5010d0775e86bcf6d6dd9ea
Primemaster <prime@DESKTOP-VEQ67I6>
2019-04-24 12:23:01 +0100

    Update CDev branch

--- src/main.cpp
+++ src/main.cpp
@@ -22,21 +22,20 @@
 //Fix an error with thread concurrency at ThreadManager.cpp : 67 -> pool erased
 //Create a system where there can be more than one .ini file, for multiple configurations
 //Fix directory non existent for data or config files
-
 int main(int argc, char* argv[])
 {
 	//Initialize default windows handle for operation
 	CFlush::InitHandle();
 
 	//Redirect cerr to file
-	std::ofstream filebuffer("logs/" + GET_VERSION_STR() + ".log");
+	/*std::ofstream filebuffer("logs/" + GET_VERSION_STR() + ".log");
 	std::streambuf *cerrbuf = std::cerr.rdbuf();
-	std::cerr.rdbuf(filebuffer.rdbuf());
+	std::cerr.rdbuf(filebuffer.rdbuf());*/
 
 	//Redirect cout to modified printf - FIXME
-	std::stringstream outbuffer;
+	/*std::stringstream outbuffer;
 	std::streambuf *outbuf = std::cout.rdbuf();
-	std::cout.rdbuf(outbuffer.rdbuf());
+	std::cout.rdbuf(outbuffer.rdbuf());*/
 
 	ThreadManager manager;
 
@@ -87,7 +86,7 @@ int main(int argc, char* argv[])
 	auto tid_1 = manager.addThread(processThread, convertToIntPointer(f, &doptions));
 	//auto tid_2 = manager.addThread(controlThread, NULL);
 
-	CFlush::FlushConsoleStream(&outbuffer);
+	//CFlush::FlushConsoleStream(&outbuffer);
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
@@ -95,158 +94,20 @@ int main(int argc, char* argv[])
 	manager.joinThreadSync(tid_1);
 	//manager.joinThreadSync(tid_2);
 
-	HANDLE coms = serial_initHandle("COM4", GENERIC_READ | GENERIC_WRITE, { 0 });
-	serial_writeBytes(coms, "1PA12.34;1WS;1TP?\r", 19);
-	char buffer[256];
-	DWORD size = 0;
-	serial_readBytes(coms, buffer, 256, &size);
-	serial_closeHandle(coms);
-
 	fclose(f);
 
 	std::cout << "Program uptime: " << Timer::apiUptimeString() << std::endl;
 
 	//CFlush::ClearConsole(0, CFlush::rows - THREAD_CONCURRENCY);
-	CFlush::FlushConsoleStream(&outbuffer);
+	//CFlush::FlushConsoleStream(&outbuffer);
 
 	//Always revert the .ini file to default for safety purposes and test only
 	IO::createIniFile("config\\default.ini");
 
-	CFlush::FlushConsoleStream(&outbuffer);
+	//CFlush::FlushConsoleStream(&outbuffer);
 
 	//Close the handle only when all user threads stopped
-	CFlush::CloseHandle();
+	//CFlush::CloseHandle();
 
-	return 0;
-}
-
-int test()
-{
-	char   ComPortName[] = "COM4";
-	BOOL Status;
-
-	HANDLE hComm = CreateFile(ComPortName,                       // Name of the Port to be Opened
-		GENERIC_READ | GENERIC_WRITE,      // Read/Write Access
-		0,                                 // No Sharing, ports cant be shared
-		NULL,                              // No Security
-		OPEN_EXISTING,                     // Open existing port only
-		0,                                 // Non Overlapped I/O
-		NULL);                             // Null for Comm Devices
-
-	if (hComm == INVALID_HANDLE_VALUE)
-		std::cout << "\n   Port " << ComPortName << " Error - Not Opened..." << std::endl;
-	else
-		std::cout << "\n   Port " << ComPortName << " Opened!" << std::endl;
-
-	DCB dcbSerialParams = { 0 };                        // Initializing DCB structure
-	dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
-
-	Status = GetCommState(hComm, &dcbSerialParams);     //retreives  the current settings
-
-	if (Status == FALSE)
-		printf("\n   Error! in GetCommState()");
-
-	dcbSerialParams.BaudRate = 921600;			// Setting BaudRate = 921.6 kBd
-	dcbSerialParams.ByteSize = 8;				// Setting ByteSize = 8
-	dcbSerialParams.StopBits = ONESTOPBIT;		// Setting StopBits = 1
-	dcbSerialParams.Parity = NOPARITY;			// Setting Parity = None 
-
-	Status = SetCommState(hComm, &dcbSerialParams);  //Configuring the port according to settings in DCB 
-
-	if (Status == FALSE)
-	{
-		printf("\n   Error! in Setting DCB Structure");
-	}
-	else
-	{
-		printf("\n   Setting DCB Structure Successfull\n");
-		printf("\n       Baudrate = %d", dcbSerialParams.BaudRate);
-		printf("\n       ByteSize = %d", dcbSerialParams.ByteSize);
-		printf("\n       StopBits = %d", dcbSerialParams.StopBits);
-		printf("\n       Parity   = %d", dcbSerialParams.Parity);
-	}
-
-	/*------------------------------------ Setting Timeouts --------------------------------------------------*/
-
-	COMMTIMEOUTS timeouts = { 0 };
-
-	timeouts.ReadIntervalTimeout = 50;
-	timeouts.ReadTotalTimeoutConstant = 50;
-	timeouts.ReadTotalTimeoutMultiplier = 10;
-	timeouts.WriteTotalTimeoutConstant = 50;
-	timeouts.WriteTotalTimeoutMultiplier = 10;
-
-	if (SetCommTimeouts(hComm, &timeouts) == FALSE)
-		printf("\n   Error! in Setting Time Outs");
-	else
-		printf("\n\n   Setting Serial Port Timeouts Successfull");
-
-	char   lpBuffer[] = "1VA15;1PA0;1WS1;1TP?\r";// lpBuffer should be  char or byte array, otherwise write wil fail
-	DWORD  dNoOFBytestoWrite;               // No of bytes to write into the port
-	DWORD  dNoOfBytesWritten = 0;           // No of bytes written to the port
-
-	dNoOFBytestoWrite = sizeof(lpBuffer); // Calculating the no of bytes to write into the port
-
-	Status = WriteFile(hComm,               // Handle to the Serialport
-		lpBuffer,            // Data to be written to the port 
-		dNoOFBytestoWrite,   // No of bytes to write into the port
-		&dNoOfBytesWritten,  // No of bytes written to the port
-		NULL);
-
-	if (Status == TRUE)
-		printf("\n\n    %s - Written to %s", lpBuffer, ComPortName);
-	else
-		printf("\n\n   Error %d in Writing to Serial Port\n", GetLastError());
-
-	DWORD dwEventMask = 0;                     // Event mask to trigger
-	char  TempChar = 0;                        // Temporary Character
-	char  SerialBuffer[256];				   // Buffer Containing Rxed Data
-	DWORD NoBytesRead = 0;                     // Bytes read by ReadFile()
-	int i = 0;
-
-	Status = SetCommMask(hComm, EV_RXCHAR); //Configure Windows to Monitor the serial device for Character Reception
-
-	if (Status == FALSE)
-		printf("\n\n    Error! in Setting CommMask");
-	else
-		printf("\n\n    Setting CommMask successfull");
-
-
-	/*------------------------------------ Setting WaitComm() Event   ----------------------------------------*/
-
-	printf("\n\n    Waiting for Data Reception");
-
-	Status = WaitCommEvent(hComm, &dwEventMask, NULL); //Wait for the character to be received
-
-	/*-------------------------- Program will Wait here till a Character is received ------------------------*/
-
-	if (Status == FALSE)
-	{
-		printf("\n    Error! in Setting WaitCommEvent()");
-	}
-	else //If  WaitCommEvent()==True Read the RXed data using ReadFile();
-	{
-		printf("\n\n    Characters Received = ");
-		while(true)
-		{
-			if (!ReadFile(hComm, &TempChar, sizeof(TempChar), &NoBytesRead, NULL))
-			{
-				break;
-			}
-			SerialBuffer[i] = TempChar;
-			i++;
-			if (TempChar == '\n') break;
-		}
-
-
-		/*------------Printing the RXed String to Console----------------------*/
-
-		int j = 0;
-		for (j = 0; j < i - 1; j++)		// j < i-1 to remove the dupliated last character
-			printf("%c", SerialBuffer[j]);
-		putchar('\n');
-	}
-
-	CloseHandle(hComm);//Closing the Serial Port
 	return 0;
 }

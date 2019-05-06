7707cbcd18ec07f508656581a6e118573a97ccda
tamartins <35746232+ta-martins@users.noreply.github.com>
2019-05-06 23:56:25 +0100

    Add files via upload

--- Intideas/Main.py
+++ Intideas/Main.py
@@ -7,22 +7,28 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from NewRec import *
-from Records import *
+from NewRec import Ui_MainWindow1
+from Records import Ui_MainWindow2
+
 
 class Ui_MainWindow(object):
 
     def a1(self):
         self.window=QtWidgets.QMainWindow()
-        self.ui2=Ui_MainWindow1()
+        self.ui1=Ui_MainWindow1()
+        self.ui1.setup(self.window)
+        self.window.show()
+
+
+
+    def a2(self):
+        self.window = QtWidgets.QMainWindow()
+        self.ui2 = Ui_MainWindow2()
         self.ui2.setup(self.window)
         self.window.show()
 
-    def a2(self):
-        self.window=QtWidgets.QMainWindow()
-        self.ui1=Ui_MainWindow2()
-        self.ui1.setup(self.window)
-        self.window.show()
+
+
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -36,7 +42,7 @@ class Ui_MainWindow(object):
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_2.setGeometry(QtCore.QRect(70, 180, 75, 23))
         self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_2.clicked.connect(self.a2)
+        self.pushButton.clicked.connect(self.a2)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 514, 21))
@@ -52,8 +58,10 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "New"))
+        self.pushButton.setText(_translate("MainWindow", "New Record"))
         self.pushButton_2.setText(_translate("MainWindow", "Last Record"))
+
+
 
 
 if __name__ == "__main__":
--- Intideas/Pass.py
+++ Intideas/Pass.py
@@ -1,39 +1,51 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\Utilizador\Desktop\NewRec.ui'
+# Form implementation generated from reading ui file 'C:\Users\Utilizador\Desktop\Main.ui'
 #
 # Created by: PyQt5 UI code generator 5.11.3
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from NewRec import Ui_MainWindow1
+from Records import Ui_MainWindow2
 
-class Ui_MainWindow1(object):
 
-    def a14(self):
+class Ui_MainWindow(object):
+
+    def a1(self):
         self.window=QtWidgets.QMainWindow()
-        self.ui=Ui_MainWindow()
-        self.ui.setupUi(self.window)
+        self.ui1=Ui_MainWindow1()
+        self.ui1.setup(self.window)
         self.window.show()
-        MainWindow.hide()
+
+
+
+    def a2(self):
+        self.window = QtWidgets.QMainWindow()
+        self.ui2 = Ui_MainWindow2()
+        self.ui2.setup(self.window)
+        self.window.show()
 
 
 
 
-    def setup(self, MainWindow):
+    def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(514, 265)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(260, 440, 75, 23))
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(440, 440, 131, 23))
+        self.pushButton.setGeometry(QtCore.QRect(210, 180, 71, 21))
         self.pushButton.setObjectName("pushButton")
-        self.pushButton.clicked.connect(self.a14)
+        self.pushButton.clicked.connect(self.a1)
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(70, 180, 75, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.clicked.connect(self.a2)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 514, 21))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -46,16 +58,18 @@ class Ui_MainWindow1(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton_2.setText(_translate("MainWindow", "Plot"))
-        self.pushButton.setText(_translate("MainWindow", "Return to Main Menu"))
+        self.pushButton.setText(_translate("MainWindow", "New Record"))
+        self.pushButton_2.setText(_translate("MainWindow", "Last Record"))
+
+
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui1 = Ui_MainWindow1()
-    ui1.setup(MainWindow)
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
 

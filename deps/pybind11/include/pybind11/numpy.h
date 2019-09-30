88a97c26971d5ca32b871e30cd6043b3f5ce6a19
Primemaster <cesarsky5@gmail.com>
2019-09-30 20:57:51 +0100

    Fixed pybind11 with winAPI MINMAX

--- deps/pybind11/include/pybind11/numpy.h
+++ deps/pybind11/include/pybind11/numpy.h
@@ -1391,7 +1391,7 @@ enum class broadcast_trivial { non_trivial, c_trivial, f_trivial };
 template <size_t N>
 broadcast_trivial broadcast(const std::array<buffer_info, N> &buffers, ssize_t &ndim, std::vector<ssize_t> &shape) {
     ndim = std::accumulate(buffers.begin(), buffers.end(), ssize_t(0), [](ssize_t res, const buffer_info &buf) {
-        return std::max(res, buf.ndim);
+        return (std::max)(res, buf.ndim);
     });
 
     shape.clear();

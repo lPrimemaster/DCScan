355b089cbe64157951379e110b7181107393fee8
Primemaster <cesarsky5@gmail.com>
2019-09-30 21:29:02 +0100

    delete old files

--- src/core/api/include/DCScan.h
+++ src/core/api/include/DCScan.h
@@ -1,46 +1,22 @@
-#ifdef _API_EXPORTING
-#define DCS_API  __declspec(dllexport)
-#else
-#define DCS_API  __declspec(dllimport)
-#endif
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "../worker_threads.h"
+#include "../base/Timer.h"
+#include "../base/counter.h"
 
-//All extern definitions must be visible here
+#include "../../ctrl/CFlush.h"
 
-#define DCS_FIXED_DURATION 0x00
-#define DCS_UNDEF_DURATION 0x01
+#include "../io/usb_serial.h"
+#include "../io/register.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include "../../version.h"
 
-	struct _dcs_acquisition
-	{
-		char filename[64];
-	};
+#include "../io/SerialCom.h"
 
-	struct _dcs_stupoptions
-	{
-		//placeholder for an interop struct for the default settings
-		int a;
-	};
+#include "../../ctrl/PerfCount.h" //Test only
+#include "../../ctrl/PyScript.h"  //Test only
 
-	typedef struct _dcs_acquisition DCScan_Context;
-	typedef struct _dcs_stupoptions DCScan_Settings;
+//Define PYMODULES for export
 
-	/* Starts the DSC Acquisition process remotely */
-	DCS_API void DCScan_StartAcq(DCScan_Context* acquisition);
-
-	/* Stops the DSC Acquisition process remotely */
-	DCS_API void DCScan_StopAcq(DCScan_Context* acquisition);
-
-	/* Tells the C API what settings to store in the .ini file (or registry) for the next startup */
-	DCS_API void DCScan_StartupSettings(DCScan_Settings* settings);
-
-	/* Creates a new context to work with, if flags is NULL then all the default values are loaded from the default .ini file */
-	DCS_API DCScan_Context* DCScan_CreateAcqContext(const char* config);
-
-	/* Deletes a context */
-	DCS_API void DCScan_FreeAcqContext(DCScan_Context* acquisition);
-#ifdef __cplusplus
-}
-#endif
